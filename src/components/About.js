75326d36ba92c6c04326e47e5efbda7d43aadd19
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-07-11 17:28:26 -0400

    fixed about-image.jpg

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './About.css';
 
-//import aboutImage from './about-image.jpg';
+import aboutImage from './about-image.jpg';
 
 
 function About() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ import './Header.css';
     )
 };
 
-export default Header();
+export default Header;
 
 //Header component then Nav component
 //Responsive
